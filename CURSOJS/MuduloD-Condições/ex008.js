7a0b14bd3413912ba45a97e02f2524b602a0f9f8
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-01-27 12:45:14 -0300

    atrualizações

---
+++ CURSOJS/MuduloD-Condições/ex008.js
@@ -0,0 +1,12 @@
+console.log('funciounou corretamente')
+
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}km`)
+// precisamos entender que esses comandos são sequenciais ou seja não tem como eu executar um sem o outro, ainda mais na relação do console que pede a var 
+
+//Condição
+
+if(vel > 60) {
+    console.log('você ultrapassou a velocidade permitida')
+
+}  console.log('dirija sempre usando cinto de segurança')
