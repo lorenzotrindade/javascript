2ee4589da11ca3ee3eeb3dc53c5ca30c9dd3b92f
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-02-04 12:49:21 -0300

    ok

--- CURSOJS/MuduloD-Condições/EX0015/arquivo.js
+++ CURSOJS/MuduloD-Condições/EX0015/arquivo.js
@@ -1,1 +1,8 @@
-window.alert('olá user')
+function veriricar(){
+
+    var date= new date()
+    var ano= data.GetFullYear()
+    var fano=document.getElementById("txtano")
+    var res=document.querySelector('div#res')
+    if (fano.value.length == 0  ) 
+}
