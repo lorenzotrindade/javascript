6b32566b1a0d938788ce3299de10d2a45c6e9453
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-02-12 19:47:07 -0300

    ok

--- CURSOJS/MuduloD-Condições/EX0015/arquivo.js
+++ CURSOJS/MuduloD-Condições/EX0015/arquivo.js
@@ -1,8 +1,13 @@
-function veriricar(){
+function veriricar() {
 
     var date= new date()
     var ano= data.GetFullYear()
-    var fano=document.getElementById("txtano")
+    var fano =document.getElementById('txtano')
+    /*ISSO AQUI ' FANO', É UMA VARIAVEL*/ 
     var res=document.querySelector('div#res')
-    if (fano.value.length == 0  ) 
+    if (fano.value.length == 0 || fano > ano ) {
+         window.alert('ERRO, insira os dados novamente') 
+    } else {
+            window.alert('tudo ok')
+        }
 }
