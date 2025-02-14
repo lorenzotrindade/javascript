8f466b284122feec2c11de2185de20ee03e38488
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-02-14 11:30:33 -0300

    OK

--- CURSOJS/MuduloD-Condições/EX0015/arquivo.js
+++ CURSOJS/MuduloD-Condições/EX0015/arquivo.js
@@ -4,9 +4,10 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector("div#res")
 
-    if(fano && fano.value && fano.value.length <= 0)
+    if(fano.value.length <= 0 || Number(fano > ano  ))
     {window.alert('Digite outra data')}
-    else {
+    else 
+    {
 
         var fsex= document.getElementByName('radsex')
         var idade = ano - Number(fano.value)
