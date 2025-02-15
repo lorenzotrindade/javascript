f7e6d6307e8bf43b51121f54af51e6d51e46d6ec
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-02-14 21:51:04 -0300

    atualizasções

--- CURSOJS/MuduloD-Condições/EX0015/arquivo.js
+++ CURSOJS/MuduloD-Condições/EX0015/arquivo.js
@@ -1,17 +1,20 @@
 function verificar() {
-    var data = new Date()
-    var ano =  data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector("div#res")
+    var data = new Date();
+    var ano =  data.getFullYear();
+    var fano = document.getElementById('txtano');
+    var res = document.querySelector("div#res");
 
-    if(fano.value.length <= 0 || Number(fano > ano  ))
-    {window.alert('Digite outra data')}
+    if(fano.value.length <= 0 || Number(fano.value)>ano)
+    {window.alert('Digite outra data');
+
+    }
     else 
     {
 
-        var fsex= document.getElementByName('radsex')
-        var idade = ano - Number(fano.value)
+        var fsex= document.getElementByName('[name = "radsex"]: cheked')
 
-        res.innerHTML = `idade calculada: ${idade}`
+        var idade = ano - Number(fano.value);
+
+        res.innerHTML = `idade calculada: ${idade}`;
     }
 }
