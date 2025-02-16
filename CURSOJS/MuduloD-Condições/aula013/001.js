ed1e3e1337d1e2e79bd4bd89de8990ce01dd9426
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-02-16 16:47:13 -0300

    ok

---
+++ CURSOJS/MuduloD-Condições/aula013/001.js
@@ -0,0 +1,14 @@
+var c =  1
+while (c < 6) {
+    console.log(`Passo ${c}`)
+    c++
+    // tá, mas se precisa colcoar uma crescente, se não ficará sempre a mesma coisa
+    // place roder, mo legal
+}
+
+
+
+
+/*
+console.log('tudo bem')
+*/
