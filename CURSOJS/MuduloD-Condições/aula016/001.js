13a7d7df68ba20ddecf5d3352938a1ec5a2d4bbe
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-02-16 17:39:30 -0300

    ok

--- CURSOJS/MuduloD-Condições/aula016/001.js
+++ CURSOJS/MuduloD-Condições/aula016/001.js
@@ -1,14 +1,18 @@
 var c =  1
 while (c < 6) {
     console.log(`Passo ${c}`)
-    c++
+    c++ 
     // tá, mas se precisa colcoar uma crescente, se não ficará sempre a mesma coisa
     // place roder, mo legal
 }
-
-
-
-
 /*
 console.log('tudo bem')
 */
+
+var c = 1
+do {
+console.log(`Passo ${c}`) 
+c++
+} while (c <= 6) {
+    
+}
---
+++ CURSOJS/MuduloD-Condições/aula016/002.js
@@ -0,0 +1,12 @@
+// var c = 1
+// while (c <= 5) {
+//     console.log(`Passo ${c}`)
+//      c++ 
+//     }
+
+    // o mesmo codigo em FOR
+    console.log('Vai começar..')
+    for(var c= 1;c <=5;c++) {
+        console.log(`Passo ${c}`)
+    }
+    console.log('Fim')
