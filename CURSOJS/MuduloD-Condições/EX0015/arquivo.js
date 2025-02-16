ebfc776f7cc62800bb388962872a9a94e66046cc
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-02-16 12:24:07 -0300

    ok

--- CURSOJS/MuduloD-Condições/EX0015/arquivo.js
+++ CURSOJS/MuduloD-Condições/EX0015/arquivo.js
@@ -15,16 +15,62 @@ function verificar() {
         var idade = ano - Number(fano.value);
         // para teste
         // res.innerHTML = `idade calculada: ${idade}`;
+        var genero = ''
+        var img = document.createElement('img')
+            img.setAttribute('id', 'foto')
+            // isso aqui acima é a mesma coisa que: <img id='foto'></img>
+
+        
         if (fsex[0].cheked) {
             genero = 'homem' 
+            if (idade >= 0 && idade <10 ) {
+                //CRIANÇA
+                img.setAttribute('src','BEBEMENINO.png')
+
+            } else if (idade >= 11 && idade <=21) {
+                //jovem
+                img.setAttribute('src','ADOLESCENTEHOMEM.png')
+
+            } 
+            else if (idade >=22 && idade < 50) {
+                //adulto
+                img.setAttribute('src','HOMEM.png')
+            } else { 
+                //idoso
+                img.setAttribute('src','IDOSO')
+                 }
         }
 
         else if (fsex[1].checked) {
             genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src','BEBEMENINA.png')
+                 }
+            } 
+            else if (idade <  21){
+                //jovem
+                img.setAttribute('src','ADOLESCENTEMULHER.png')
+                 }
+            
+            else if (idade < 50) {
+                //adulto
+                img.setAttribute('src','MULHER.png')
+                 }
+            
+            else {
+                //idoso
+                img.setAttribute('src','IDOSA.png')
+                 }
+
+                 res.Style.textAlign = 'Center' 
+                 res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+
+                 //metodo de adicinar elementos a variavel
+                 res.appendChild(img)
+            } 
         }
 
-        res.Style.textAlign = 'Center' 
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         
-    }
-}
+        
