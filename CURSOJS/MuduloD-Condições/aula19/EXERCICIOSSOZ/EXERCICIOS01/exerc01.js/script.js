54c29688b78e7c226c877688e166883a679a8a96
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-01 10:53:04 -0300

    ok

--- CURSOJS/MuduloD-Condições/aula19/EXERCICIOSSOZ/EXERCICIOS01/exerc01.js/script.js
+++ CURSOJS/MuduloD-Condições/aula19/EXERCICIOSSOZ/EXERCICIOS01/exerc01.js/script.js
@@ -2,7 +2,7 @@ let num = document.getElementById('fnum')
 var list = document.getElementById('list')
 let res = document.getElementById('res')
 let n1 = 1
-let valores = []
+let valores = [n1]
 
 
 function isNumero(n) {
@@ -12,8 +12,8 @@ function isNumero(n) {
 }
 
 // da para fazer com um for isso aqui
-function inLista(n,valores)
-    if(valores.indexOf(Number(n)) != -1) {
+function inLista(n,n1) {
+    if(n1.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
@@ -33,4 +33,38 @@ function Adicionar() {
     else {
     window.alert('Valor digitado inválido u já consta na lista')
 }
+num.value=''
+//num.value, é o nume da variavel e value, e o valor que definimos, que agora é vazio ('')
+num.focus() //essa é uma ação que faz vc voltar o cursor para a variavel ali definida
 }
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('adicione algo')
+    } else {
+        let tot = valores.length 
+        let maior =  valores[0]
+        let menor = valores[0]
+        let soma = 0 
+        let media = 0
+
+        for (let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor= valores[pos]
+            }
+        }
+        res.innerHTML =''
+        res.innerHTML += `<p>Ao todo, temos ${tot} tantos numeros cadastrados</p>`
+
+        // apresentando em tela 
+
+        res.innerHTML= `<p>O maior valor informado é ${maior} </p>`
+        res.innerHTML = `<p> o menor valor informado é ${menor}</p´>`
+        res.innerHTML = `<p> A soma de todos os valores é ${soma} temos.</p>`
+    }
+}
+
