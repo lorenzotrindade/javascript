73b71c07a71287dd74bdd1d9200cba9cbd11aecd
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-01 21:55:53 -0300

    ok

---
+++ CURSOJS/MuduloD-Condições/aula20/ob001.js
@@ -0,0 +1,13 @@
+let amigo = 
+// por usar {{}} JS entende como um objeto
+{
+    // aqui temos: paramatro nome, com valor jose
+    nome: 'josé', 
+    // em baixo a mesma coisa, mas repare nos typos
+    pesp: 85.4, 
+    sexo:"M",
+    // ainda podemos declarar uma função dentro do objeto, o que seria um METODO
+     engordar(p){
+
+}}
+console.log( amigo)
