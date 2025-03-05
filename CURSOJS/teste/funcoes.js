ada6788086bfe17dc143c2d523622104632c1d92
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-05 10:56:22 -0300

    41

--- CURSOJS/teste/funcoes.js
+++ CURSOJS/teste/funcoes.js
@@ -1,11 +1,17 @@
 var ini = document.getElementById('numI')
 var final = document.getElementById('numF')
-var pas = document.getElementById('passo')
+var pass = document.getElementById('passo')
 var resultado = document.getElementById('res')
+var lista =  documento.getElementById('lista')
 
 function Calcular () {
-    if( Number(ini.value.length) == 0 || Number(final.value.length) == 0 || Number(pas.value.length)) {
-        window.alert('digite um valor') }
-        else {window.alert('tudo ok')}
+    if( ini.value.length == 0 && Number(ini.value) <= 100
+    || final.value.length == 0 && Number(final.value) <= 100 || 
+    pass.value.length == 0 && Number(pass.value) <= 100) 
+    {
+        lista.innerHTML=('O resultado está valido')
+     }
+        else {resultado.innerHTML=('O resultado é INvalido')}
+    }
 
-}
+
