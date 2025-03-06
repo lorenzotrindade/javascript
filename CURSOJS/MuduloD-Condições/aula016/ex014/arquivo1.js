9a969d44043de173a9fde482e152079ba710fe40
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-06 19:57:18 -0300

    ok

---
+++ CURSOJS/MuduloD-Condições/aula016/ex014/arquivo1.js
@@ -0,0 +1,7 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim=document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if(ini.Value.length ==0 || final.valuelength || passo.Value.length)
+}
--- CURSOJS/teste/funcoes.js
+++ CURSOJS/teste/funcoes.js
@@ -6,9 +6,9 @@ var lista =  documento.getElementById('lista')
 // mesmo tudo aqui pegando o valor de lá e mandando aoutra variavel, ainda não mudaram seu tipo que é HTML
 
 function Calcular () {
-    // if( ini.value.length == 0 && Number(ini.value) <= 100
-    // || final.value.length == 0 && Number(final.value) <= 100 || 
-    // pass.value.length == 0 && Number(pass.value) <= 100) 
+    if( ini.value.length == 0 && Number(ini.value) <= 100
+    || final.value.length == 0 && Number(final.value) <= 100 || 
+    pass.value.length == 0 && Number(pass.value) <= 100) 
     {
         var p = Number(pass.value)
         var i = Number(ini.value)
@@ -17,13 +17,13 @@ function Calcular () {
         resultado.innerHTML =`O valor ficará:`
         
         if( p < f) {
-            for(var pulo = i; pulo < f; p+=pulo) {
-                resultado.innerHTML += `${p}`
+            for(var pulo = i; pulo < f; pulo+=p) {
+                resultado.innerHTML += `${pulo}`
                 // ou sej aqui a cima estou dizendo que recebe mais ele mesmo
             } 
         }  else {
-            for(var pulo = i; pulo > f; p-=pulo) {
-                resultado.innerHTML +=`${p}`
+            for(var pulo = i; pulo > f; pulo-=p) {
+                resultado.innerHTML +=`${pulo}`
             }
 }    
      } 
@@ -31,7 +31,7 @@ function Calcular () {
         }
 
     
-    var iten = oppenchild(option)
+    
     
 
 
