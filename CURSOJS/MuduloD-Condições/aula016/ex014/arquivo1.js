a4e31ef5eb22d7f957066b2eead1377be05bbb43
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-09 11:38:50 -0300

    ok

--- CURSOJS/MuduloD-Condições/aula016/ex014/arquivo1.js
+++ CURSOJS/MuduloD-Condições/aula016/ex014/arquivo1.js
@@ -2,6 +2,19 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim=document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if(ini.Value.length ==0 || final.valuelength || passo.Value.length)
-}
+    if(ini.Value.length ==0 || final.Valuealue.length || passo.Value.length)
+        { window.alert('erro')} 
+    else {
+        res.innerHTML='contado:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for ( let c=i;c<=f;c+=p){
+            res.innerHTML+=`${c}`
+        }
+
+    }
+    }
