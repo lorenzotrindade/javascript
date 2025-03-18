22c2afbc9a7ccfdd0fa5e8d85af238fa5ac8e606
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-17 21:56:17 -0300

    ok

--- CURSOJS/teste/funcoes.js
+++ CURSOJS/teste/funcoes.js
@@ -1,37 +1,25 @@
 var ini = document.getElementById('numI')
-var final = document.getElementById('numF')
-var pass = document.getElementById('passo')
-var resultado = document.getElementById('res')
-var lista =  documento.getElementById('lista')
-// mesmo tudo aqui pegando o valor de lá e mandando aoutra variavel, ainda não mudaram seu tipo que é HTML
-
-function Calcular () {
-    if( ini.value.length == 0 && Number(ini.value) <= 100
-    || final.value.length == 0 && Number(final.value) <= 100 || 
-    pass.value.length == 0 && Number(pass.value) <= 100) 
-    {
-        var p = Number(pass.value)
-        var i = Number(ini.value)
-        var f = Number(final.value)
-
-        resultado.innerHTML =`O valor ficará:`
+var fim = document.getElementById('numF')
+var pass= document.getElementById('passo') 
+var res= document.getElementById('res')
+ 
+ function calcular() {
+    if(ini.value.length == 0 || fim.value.length==0 || pass.value.length==0 ) 
+        { 
+            window.alert("Valores incorretos")
+    } else {
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pass.value) 
+        res.innerHTML+= `Os numeros são:`
         
-        if( p < f) {
-            for(var pulo = i; pulo < f; pulo+=p) {
-                resultado.innerHTML += `${pulo}`
-                // ou sej aqui a cima estou dizendo que recebe mais ele mesmo
-            } 
-        }  else {
-            for(var pulo = i; pulo > f; pulo-=p) {
-                resultado.innerHTML +=`${pulo}`
-            }
-}    
-     } 
-        // else {window.alert('tudo errado')}
-        }
 
-    
-    
-    
+        if(p < f) {
+            for(var pulo = i;pulo<f;pulo+=p) {
+                res.innerHTML+= ` ${pulo}`
+                
+            }  
+        } else {alert('ok')}
+    }
 
-
+ }
