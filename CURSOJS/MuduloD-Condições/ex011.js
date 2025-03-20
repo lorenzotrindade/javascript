5cff30a296d60acb16d68d53f4202529c6a6bfa2
Lorenzo Trindade <lorenzotrindades@gmail.com>
2025-03-19 23:12:27 -0300

    ok

--- CURSOJS/MuduloD-Condições/ex011.js
+++ CURSOJS/MuduloD-Condições/ex011.js
@@ -1,8 +1,7 @@
-var idade = 16
-if(idade < 18) {
+var idade = 22
+if(idade < 16) {
     console.log('não vota')
-} else{
-    if(idade <=1){
-
-    }
-}
+} else if(idade >=16 && idade < 18){
+        console.log('Voto opcional')
+    } else if(idade >=18 && idade == 65)
+        console.log('obrigatorio')
