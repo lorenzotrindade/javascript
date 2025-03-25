015cec42f1f693f56b5d714b37eec800fdf133dc
Lorenzo Trindade <151590431+lorenzotrindade@users.noreply.github.com>
2025-03-25 09:45:10 -0300

    ok

--- CURSOJS/teste/calcularidade.js
+++ CURSOJS/teste/calcularidade.js
@@ -1,1 +1,18 @@
-calcularidade(){}
+calcularidade() {
+    let ano = document.getfullyear()
+    let nascimento = document.getElementById('nascimento')
+    let residade = document.getElementeById('residade')
+
+    if(ano.value.length == 0 || nascimento.value.length == 0 
+        && ano.value <= 0 && nascimento.value >= ano)  {
+            alert('valores incorretos')
+        }  else {
+            n = Number(nascimento)
+            a = Number(ano)
+
+            if(n < a){
+                return residade;
+            }
+        }
+    
+}
